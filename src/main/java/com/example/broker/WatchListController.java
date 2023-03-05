3656d421c715532332346ec733f08cc0faa43f02
JeffRisberg <j.s.risberg@gmail.com>
2023-03-04 19:56:33 -0800

    added WatchListController

--- src/main/java/com/example/broker/WatchListController.java
+++ src/main/java/com/example/broker/WatchListController.java
@@ -1,25 +1,47 @@
 package com.example.broker;
 
-import com.example.broker.model.Symbol;
+
+import com.example.broker.model.WatchList;
+import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import java.util.UUID;
 
-@Controller("/symbols")
+@Controller("/account/watchlist")
 public class WatchListController {
 
-  private final InMemoryStore inMemoryStore;
+  private static final Logger LOG = LoggerFactory.getLogger(WatchListController.class);
 
-  public WatchListController(final InMemoryStore inMemoryStore) {
-    this.inMemoryStore = inMemoryStore;
+  static final UUID ACCOUNT_ID = UUID.randomUUID();
+
+  public WatchListController() {
+    //this.store = store;
   }
 
-  @Put(consumes = JsonObject, produces = JsonObject)
-  public Watchlist update(Watchlist watchlist) {
-    to be filled in
+  @Get(produces = MediaType.APPLICATION_JSON)
+  public WatchList get() {
+    LOG.debug("get - {}", Thread.currentThread().getName());
+    //return store.getWatchList(ACCOUNT_ID);
+    return null;
+  }
 
+  @Put(
+    consumes = MediaType.APPLICATION_JSON,
+    produces = MediaType.APPLICATION_JSON
+  )
+  public WatchList update(@Body WatchList watchList) {
+    //return store.updateWatchList(ACCOUNT_ID, watchList);
+    return null;
+  }
+
+  @Delete(
+    value = "/{accountId}",
+    consumes = MediaType.APPLICATION_JSON,
+    produces = MediaType.APPLICATION_JSON
+  )
+  public void delete(@PathVariable UUID accountId) {
+    //store.deleteWatchList(accountId);
   }
 }
--- src/main/java/com/example/broker/model/WatchList.java
+++ src/main/java/com/example/broker/model/WatchList.java
@@ -1,4 +1,24 @@
 package com.example.broker.model;
 
+import io.micronaut.core.annotation.Introspected;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Introspected
 public class WatchList {
+
+  private List<Symbol> symbols = new ArrayList<>();
+
+  public WatchList(List<Symbol> symbols) {
+    this.symbols = symbols;
+  }
+
+  public List<Symbol> getSymbols() {
+    return symbols;
+  }
+
+  public void setSymbols(List<Symbol> symbols) {
+    this.symbols = symbols;
+  }
 }
